8d16606843442b179141346c8e5b82904fb30b23
Nicholas Haley <hello@nichaley.com>
2023-09-05 18:54:36 -0700

    Fix duplicate list item keys inside blog

--- apps/web/src/app/blog/[[...slug]]/page.tsx
+++ apps/web/src/app/blog/[[...slug]]/page.tsx
@@ -31,7 +31,7 @@ function BlogPage({ isError, isNode, post, posts, isNotFound }: FloePageProps) {
         <section className="w-full pt-16 mt-12 prose border-t dark:prose-invert md:pt-0 md:mt-0 border-zinc-700 md:border-0">
           {posts.map((blog) => (
             <Link
-              key={blog.slug}
+              key={blog.slug + blog.datasourceId}
               href={blog.slug}
               className="mb-2 no-underline"
             >
@@ -42,7 +42,7 @@ function BlogPage({ isError, isNode, post, posts, isNotFound }: FloePageProps) {
         <section className="relative w-full md:w-80 shrink-0">
           <div className="relative inset-0 md:absolute">
             <div className="relative w-full md:fixed md:w-80">
-              <h1 className="mb-3 font-semibold tracking-tight sm:font-bold text-4xl dark:text-white">
+              <h1 className="mb-3 text-4xl font-semibold tracking-tight sm:font-bold dark:text-white">
                 Blog
               </h1>
               <h2 className="mt-2 mb-10 text-lg leading-8 dark:text-gray-300">
