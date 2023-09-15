58d6d018ab8b9dce817e85062096830c01867dfd
Nicholas Haley <hello@nichaley.com>
2023-09-15 16:32:07 -0400

    Setup a useable dynamic colour solution

--- apps/web/src/app/[subdomain]/ThemeProvider.tsx
+++ apps/web/src/app/[subdomain]/ThemeProvider.tsx
@@ -9,9 +9,6 @@ export function ThemeProvider({
   children,
   ...props
 }: any & ThemeProviderProps) {
-  /**
-   * This creates a flicker -- need a better solution!
-   */
   const primary =
     project.appearance === "LIGHT" ? project.primary : project.primaryDark;
   const background =
@@ -19,6 +16,22 @@ export function ThemeProvider({
       ? project.background
       : project.backgroundDark;
 
+  /**
+   * Note: This is a hack to get the theme to work. The hack works by:
+   * 1) Setting the css var primary and background colours the ones from the
+   *    server in the inline style attribute. This ensures the colours looks
+   *    correct without any flicker on load.
+   * 2) Using the useEffect to also set the ones on the documentElement, so that
+   *    the document background colour is also set. Without this line, the user
+   *    would see a white background flicker before load.
+   *
+   * With the current solution, the user will very briefly see the wrong
+   * document colour if they scroll up or down very quickly after page load.
+   *
+   * A better solution might be to split the floe website from the hosted app.
+   * Then in the new hosted app, load the root of the application under the
+   * subdomain, and set the colours on the server.
+   */
   React.useEffect(() => {
     document.documentElement.style.setProperty("--color-primary", primary);
     document.documentElement.style.setProperty(
@@ -27,7 +40,21 @@ export function ThemeProvider({
     );
   }, []);
 
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <NextThemesProvider {...props}>
+      <div
+        style={
+          {
+            "--color-primary": primary,
+            "--color-background": background,
+          } as React.CSSProperties
+        }
+        className="bg-background"
+      >
+        {children}
+      </div>
+    </NextThemesProvider>
+  );
 }
 
 export default ThemeProvider;
