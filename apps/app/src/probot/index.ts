8fe8de37a85312496af0ac80f3bff05f48c3adac
Nicholas Haley <hello@nichaley.com>
2023-09-19 17:43:29 -0400

    Setup some handlers for GH checks

--- apps/app/src/probot/index.ts
+++ apps/app/src/probot/index.ts
@@ -9,6 +9,14 @@ const appHandler = (app: Probot) => {
   app.on("push", async (context) => {
     handlePushEvents(context);
   });
+
+  app.on("check_run", async (context) => {
+    console.log("check_run", context);
+  });
+
+  app.on("check_run.created", async (context) => {
+    console.log("check_run.created", context);
+  });
 };
 
 export default appHandler;
