f0978f15d0d8d9e0d826ad0dd831b6337ddd078f
Nicholas Haley <hello@nichaley.com>
2023-09-19 10:38:28 -0400

    Add inputs for social links

---
+++ apps/app/src/app/(authenticated)/[projectslug]/branding/Socials.tsx
@@ -0,0 +1,225 @@
+"use client";
+
+import { api } from "@/utils/trpc";
+import { Card } from "@/components";
+import { Input } from "@floe/ui";
+import { useProjectContext } from "@/context/project";
+import { useQueryClient } from "@tanstack/react-query";
+import { Project } from "@floe/db";
+import { useState } from "react";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+
+type FormData = {
+  twitterUrl: string | undefined;
+  githubUrl: string | undefined;
+  discordUrl: string | undefined;
+  instagramUrl: string | undefined;
+  facebookUrl: string | undefined;
+  youtubeUrl: string | undefined;
+  twitchUrl: string | undefined;
+  slackUrl: string | undefined;
+};
+
+const projectSchema = yup
+  .object({
+    twitterUrl: yup.string().url().optional(),
+    githubUrl: yup.string().url().optional(),
+    discordUrl: yup.string().url().optional(),
+    instagramUrl: yup.string().url().optional(),
+    facebookUrl: yup.string().url().optional(),
+    youtubeUrl: yup.string().url().optional(),
+    twitchUrl: yup.string().url().optional(),
+    slackUrl: yup.string().url().optional(),
+  })
+  .required();
+
+export const Socials = () => {
+  const queryClient = useQueryClient();
+  const { currentProject, queryKey } = useProjectContext();
+  const { mutateAsync, isLoading } = api.project.update.useMutation({
+    onSuccess: () => queryClient.invalidateQueries({ queryKey }),
+  });
+  const {
+    register,
+    getValues,
+    formState: { errors, isValid },
+  } = useForm<FormData>({
+    mode: "onChange",
+    resolver: yupResolver(projectSchema),
+    defaultValues: {
+      twitterUrl: currentProject?.twitterURL ?? undefined,
+      githubUrl: currentProject?.githubURL ?? undefined,
+      discordUrl: currentProject?.discordURL ?? undefined,
+      instagramUrl: currentProject?.instagramURL ?? undefined,
+      facebookUrl: currentProject?.facebookURL ?? undefined,
+      youtubeUrl: currentProject?.youtubeURL ?? undefined,
+      twitchUrl: currentProject?.twitchURL ?? undefined,
+      slackUrl: currentProject?.slackURL ?? undefined,
+    },
+  });
+  const [isSaving, setIsSaving] = useState(false);
+
+  if (!currentProject) {
+    return;
+  }
+
+  return (
+    <Card
+      title="Social links"
+      bottomActions={[
+        {
+          text: isSaving ? "Saving..." : "Save",
+          type: "submit",
+          disabled: !isValid || isSaving,
+          onClick: async () => {
+            setIsSaving(true);
+            await mutateAsync({
+              projectId: currentProject?.id,
+              ...(getValues("twitterUrl") !== currentProject?.twitterURL && {
+                primary: getValues("twitterUrl"),
+              }),
+              ...(getValues("githubUrl") !== currentProject?.githubURL && {
+                primaryDark: getValues("githubUrl"),
+              }),
+              ...(getValues("discordUrl") !== currentProject?.discordURL && {
+                background: getValues("discordUrl"),
+              }),
+              ...(getValues("instagramUrl") !==
+                currentProject?.instagramURL && {
+                backgroundDark: getValues("instagramUrl"),
+              }),
+              ...(getValues("youtubeUrl") !== currentProject?.youtubeURL && {
+                backgroundDark: getValues("youtubeUrl"),
+              }),
+              ...(getValues("facebookUrl") !== currentProject?.facebookURL && {
+                backgroundDark: getValues("facebookUrl"),
+              }),
+              ...(getValues("twitchUrl") !== currentProject?.twitchURL && {
+                backgroundDark: getValues("twitchUrl"),
+              }),
+            }).finally(() => setIsSaving(false));
+          },
+        },
+      ]}
+    >
+      <div className="flex flex-col gap-8">
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              GitHub Url
+            </h4>
+            <p className="text-sm text-gray-500">
+              Url to you GitHub account or repository.
+            </p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://github.com/Floe-dev"
+              errortext={errors.githubUrl?.message}
+              {...register("githubUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Twitter Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Twitter account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://twitter.com/Floe_dev"
+              errortext={errors.twitchUrl?.message}
+              {...register("twitterUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Discord Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Discord account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://github.com/Floe-dev/floe"
+              errortext={errors.discordUrl?.message}
+              {...register("discordUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Slack Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Slack account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://join.slack.com/t/floedev/shared_invite/123"
+              errortext={errors.slackUrl?.message}
+              {...register("slackUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Twitch Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Twitch account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://www.twitch.tv/floedev"
+              errortext={errors.twitchUrl?.message}
+              {...register("twitchUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Youtube Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Youtube account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://www.youtube.com/watch?v=1bZnhkwl6_s"
+              errortext={errors.youtubeUrl?.message}
+              {...register("youtubeUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+        <div className="flex justify-between w-full">
+          <span className="flex flex-col flex-grow mr-20">
+            <h4 className="text-sm font-medium leading-6 text-gray-900">
+              Facebook Url
+            </h4>
+            <p className="text-sm text-gray-500">Your Facebook account Url.</p>
+          </span>
+          <span className="flex gap-2">
+            <Input
+              placeholder="eg. https://www.facebook.com/groups/123"
+              errortext={errors.youtubeUrl?.message}
+              {...register("youtubeUrl")}
+              disabled={isLoading}
+            />
+          </span>
+        </div>
+      </div>
+    </Card>
+  );
+};
--- apps/app/src/app/(authenticated)/[projectslug]/branding/page.tsx
+++ apps/app/src/app/(authenticated)/[projectslug]/branding/page.tsx
@@ -10,6 +10,7 @@ import { useState } from "react";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { useForm } from "react-hook-form";
+import { Socials } from "./Socials";
 
 const apperances: {
   name: string;
@@ -42,14 +43,6 @@ type FormData = {
   primaryDark: string | undefined;
   background: string | undefined;
   backgroundDark: string | undefined;
-  twitterUrl: string | undefined;
-  githubUrl: string | undefined;
-  discordUrl: string | undefined;
-  instagramUrl: string | undefined;
-  facebookUrl: string | undefined;
-  youtubeUrl: string | undefined;
-  twitchUrl: string | undefined;
-  slackUrl: string | undefined;
 };
 
 const projectSchema = yup
@@ -70,14 +63,6 @@ const projectSchema = yup
       .string()
       .matches(/^#[0-9A-F]{6}$/i)
       .optional(),
-    twitterUrl: yup.string().url().optional(),
-    githubUrl: yup.string().url().optional(),
-    discordUrl: yup.string().url().optional(),
-    instagramUrl: yup.string().url().optional(),
-    facebookUrl: yup.string().url().optional(),
-    youtubeUrl: yup.string().url().optional(),
-    twitchUrl: yup.string().url().optional(),
-    slackUrl: yup.string().url().optional(),
   })
   .required();
 
@@ -99,14 +84,6 @@ export default function Page() {
       primaryDark: currentProject?.primaryDark ?? undefined,
       background: currentProject?.background ?? undefined,
       backgroundDark: currentProject?.backgroundDark ?? undefined,
-      twitterUrl: currentProject?.twitterURL ?? undefined,
-      githubUrl: currentProject?.githubURL ?? undefined,
-      discordUrl: currentProject?.discordURL ?? undefined,
-      instagramUrl: currentProject?.instagramURL ?? undefined,
-      facebookUrl: currentProject?.facebookURL ?? undefined,
-      youtubeUrl: currentProject?.youtubeURL ?? undefined,
-      twitchUrl: currentProject?.twitchURL ?? undefined,
-      slackUrl: currentProject?.slackURL ?? undefined,
     },
   });
   const [selectedAppearance, setSelectedAppearance] = useState<
@@ -268,114 +245,7 @@ export default function Page() {
           </div>
         </div>
       </Card>
-      <Card
-        title="Social links"
-        bottomActions={[
-          {
-            text: isSaving ? "Saving..." : "Save",
-            type: "submit",
-            disabled: !isValid || isSaving,
-            onClick: async () => {
-              setIsSaving(true);
-              await mutateAsync({
-                projectId: currentProject?.id,
-                ...(currentProject?.appearance !== selectedAppearance && {
-                  appearance: selectedAppearance,
-                }),
-                ...(getValues("primary") !== currentProject?.primary && {
-                  primary: getValues("primary"),
-                }),
-                ...(getValues("primaryDark") !==
-                  currentProject?.primaryDark && {
-                  primaryDark: getValues("primaryDark"),
-                }),
-                ...(getValues("background") !== currentProject?.background && {
-                  background: getValues("background"),
-                }),
-                ...(getValues("backgroundDark") !==
-                  currentProject?.backgroundDark && {
-                  backgroundDark: getValues("backgroundDark"),
-                }),
-              }).finally(() => setIsSaving(false));
-            },
-          },
-        ]}
-      >
-        <div className="flex flex-col gap-8">
-          <div className="flex justify-between w-full">
-            <span className="flex flex-col flex-grow mr-20">
-              <h4 className="text-sm font-medium leading-6 text-gray-900">
-                Twitter Url
-              </h4>
-              <p className="text-sm text-gray-500">
-                Twitter compant account URL.
-              </p>
-            </span>
-            <span className="flex gap-2">
-              <Input
-                placeholder="eg. https://twitter.com/Floe_dev"
-                errortext={errors.twitchUrl?.message}
-                {...register("twitterUrl")}
-                disabled={isLoading}
-              />
-            </span>
-          </div>
-          <div className="flex justify-between w-full">
-            <span className="flex flex-col flex-grow mr-20">
-              <h4 className="text-sm font-medium leading-6 text-gray-900">
-                Primary color (dark)
-              </h4>
-              <p className="text-sm text-gray-500">
-                The primary color for buttons, links, etc in dark mode.
-              </p>
-            </span>
-            <span className="flex gap-2">
-              <Input
-                placeholder="eg. #4138c2"
-                errortext={errors.primaryDark?.message}
-                {...register("primaryDark")}
-                disabled={isLoading}
-              />
-            </span>
-          </div>
-          <div className="flex justify-between w-full">
-            <span className="flex flex-col flex-grow mr-20">
-              <h4 className="text-sm font-medium leading-6 text-gray-900">
-                Background color
-              </h4>
-              <p className="text-sm text-gray-500">
-                The background color in light mode.
-              </p>
-            </span>
-            <span className="flex gap-2">
-              <Input
-                placeholder="eg. #ffffff"
-                errortext={errors.background?.message}
-                {...register("background")}
-                disabled={isLoading}
-              />
-            </span>
-          </div>
-          <div className="flex justify-between w-full">
-            <span className="flex flex-col flex-grow mr-20">
-              <h4 className="text-sm font-medium leading-6 text-gray-900">
-                Background color (dark)
-              </h4>
-              <p className="text-sm text-gray-500">
-                The background color in dark mode.
-              </p>
-            </span>
-            <span className="flex gap-2">
-              <Input
-                placeholder="eg. #09090b"
-                errortext={errors.backgroundDark?.message}
-                {...register("backgroundDark")}
-                disabled={isLoading}
-              />
-            </span>
-          </div>
-        </div>
-      </Card>
+      <Socials />
     </div>
   );
 }
