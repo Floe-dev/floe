ef80a95c8c464b9e402dca7994d06392b6f1271f
Nicholas Haley <hello@nichaley.com>
2023-09-20 09:50:28 -0400

    Initial config

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { Command } from "commander";
+const program = new Command();
+console.log("Testing!");
---
+++ packages/cli/src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { Command } from "commander";
+const program = new Command();
+
+console.log("Testing!");
