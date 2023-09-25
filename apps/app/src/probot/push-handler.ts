d7cbcbb666fb601f273874db4037c8a248aabc68
Nicholas Haley <hello@nichaley.com>
2023-09-24 22:03:50 -0400

    Switch to postgres and digital ocean (#17)

--- apps/app/src/probot/push-handler.ts
+++ apps/app/src/probot/push-handler.ts
@@ -34,8 +34,6 @@ export async function handlePushEvents(context: Context<"push">) {
     return;
   }
 
-  console.log("GET COMMITS");
-
   const commits = await Promise.all(
     context.payload.commits.map((c) => {
       return context.octokit.repos.getCommit({
@@ -44,42 +42,31 @@ export async function handlePushEvents(context: Context<"push">) {
         owner,
       });
     })
-  ).catch((e) => {
-    console.error("COULD NOT GET COMMITS: ", e);
-  });
+  );
 
-  if (!commits?.length) {
-    console.log("No commits found");
-    return;
-  }
-
-  const files = commits?.map((c) => c.data.files).flat();
+  const files = commits.map((c) => c.data.files).flat();
 
   console.log("NUMBER OF FILES FOUND: ", files.length);
 
-  await Promise.all(
-    files.map(async (file) => {
-      if (!file) return;
+  files.forEach(async (file) => {
+    if (!file) return;
 
-      const isValidPost =
-        minimatch(file.filename, ".floe/**/*.md") &&
-        !minimatch(file.filename, ".floe/public/*");
+    const isValidPost =
+      minimatch(file.filename, ".floe/**/*.md") &&
+      !minimatch(file.filename, ".floe/public/*");
 
-      /**
-       * POST HANDLERS
-       */
-      if (!isValidPost) {
-        console.log("POST IS NOT VALID");
-        return;
-      }
+    /**
+     * POST HANDLERS
+     */
+    if (!isValidPost) {
+      console.log("POST IS NOT VALID");
+      return;
+    }
 
-      await Promise.all(
-        datasources.map(async (dataSource) => {
-          handlePush(file, dataSource.id);
-        })
-      );
-    })
-  );
+    datasources.forEach(async (dataSource) => {
+      handlePush(file, dataSource.id);
+    });
+  });
 }
 
 async function handlePush(
@@ -93,7 +80,7 @@ async function handlePush(
    * HANDLE FILE ADDED
    */
   if (file.status === "added") {
-    return await prisma.post
+    await prisma.post
       .upsert({
         where: {
           unique_post: {
@@ -119,7 +106,7 @@ async function handlePush(
    * HANDLE FILE RENAMED
    */
   if (file.status === "renamed" && file.previous_filename) {
-    return await prisma.post
+    await prisma.post
       .upsert({
         where: {
           unique_post: {
@@ -136,7 +123,6 @@ async function handlePush(
         },
       })
       .catch((e) => {
-        console.log(111111, file.filename, datasourceId);
         console.error("COULD NOT UPDATE POST: ", e);
       });
   }
@@ -145,14 +131,13 @@ async function handlePush(
    * HANDLE FILE MODIFIED
    */
   if (file.status === "modified") {
-    return;
   }
 
   /**
    * HANDLE FILE REMOVED
    */
   if (file.status === "removed") {
-    return await prisma.post
+    await prisma.post
       .delete({
         where: {
           unique_post: {
