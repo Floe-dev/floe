570f113b6de48d0d86ce7c9bb8f5edd0f827c08a
Nicholas Haley <hello@nichaley.com>
2023-09-24 20:33:55 -0400

    test

--- apps/app/src/probot/index.ts
+++ apps/app/src/probot/index.ts
@@ -1,5 +1,8 @@
 import prisma from "@floe/db";
 import { Probot } from "probot";
+import { Context } from "probot";
+import { minimatch } from "minimatch";
+import { Endpoints } from "@floe/utils";
 import { handlePushEvents } from "./push-handler";
 
 const appHandler = (app: Probot) => {
@@ -15,6 +18,8 @@ const appHandler = (app: Probot) => {
     const repo = context.payload.repository.name;
     const branch = context.payload.ref.replace("refs/heads/", "");
 
+    const datasources = [{ id: "clmy3mghd0001ky07j5g086vm" }];
+
     const commits = await Promise.all(
       context.payload.commits.map((c) => {
         return context.octokit.repos.getCommit({
@@ -27,8 +32,124 @@ const appHandler = (app: Probot) => {
       console.error("COULD NOT GET COMMITS: ", e);
     });
 
-    console.log(1111111, commits);
+    if (!commits?.length) {
+      console.log("No commits found");
+      return;
+    }
+
+    const files = commits?.map((c) => c.data.files).flat();
+
+    console.log("NUMBER OF FILES FOUND: ", files.length);
+
+    await Promise.all(
+      files.map(async (file) => {
+        if (!file) return;
+
+        const isValidPost =
+          minimatch(file.filename, ".floe/**/*.md") &&
+          !minimatch(file.filename, ".floe/public/*");
+
+        /**
+         * POST HANDLERS
+         */
+        if (!isValidPost) {
+          console.log("POST IS NOT VALID");
+          return;
+        }
+
+        await Promise.all(
+          datasources.map(async (dataSource) => {
+            handlePush(file, dataSource.id);
+          })
+        );
+      })
+    );
   });
 };
 
 export default appHandler;
+
+async function handlePush(
+  file: NonNullable<
+    Endpoints["GET /repos/{owner}/{repo}/commits/{ref}"]["response"]["data"]["files"]
+  >[0],
+  datasourceId: string
+) {
+  console.log("PROCESSING FILE: ", file);
+  /**
+   * HANDLE FILE ADDED
+   */
+  if (file.status === "added") {
+    return await prisma.post
+      .upsert({
+        where: {
+          unique_post: {
+            datasourceId,
+            filename: file.filename,
+          },
+        },
+        create: {
+          datasourceId,
+          filename: file.filename,
+        },
+        // This case shouldn't ever happen, but just in case
+        update: {
+          filename: file.filename,
+        },
+      })
+      .catch((e) => {
+        console.error("COULD NOT CREATE POST: ", e);
+      });
+  }
+
+  /**
+   * HANDLE FILE RENAMED
+   */
+  if (file.status === "renamed" && file.previous_filename) {
+    return await prisma.post
+      .upsert({
+        where: {
+          unique_post: {
+            datasourceId,
+            filename: file.previous_filename,
+          },
+        },
+        create: {
+          datasourceId,
+          filename: file.filename,
+        },
+        update: {
+          filename: file.filename,
+        },
+      })
+      .catch((e) => {
+        console.log(111111, file.filename, datasourceId);
+        console.error("COULD NOT UPDATE POST: ", e);
+      });
+  }
+
+  /**
+   * HANDLE FILE MODIFIED
+   */
+  if (file.status === "modified") {
+    return;
+  }
+
+  /**
+   * HANDLE FILE REMOVED
+   */
+  if (file.status === "removed") {
+    return await prisma.post
+      .delete({
+        where: {
+          unique_post: {
+            datasourceId,
+            filename: file.filename,
+          },
+        },
+      })
+      .catch((e) => {
+        console.error("COULD NOT DELETE POST: ", e);
+      });
+  }
+}
