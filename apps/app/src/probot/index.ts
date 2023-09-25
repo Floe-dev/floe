5fa6e2906d38919d691a8d3efe5f26f092df148d
Nicholas Haley <hello@nichaley.com>
2023-09-24 20:12:58 -0400

    Webhook fixes 3 (#16)
    * Testing
    
    * Try logging project

--- apps/app/src/probot/index.ts
+++ apps/app/src/probot/index.ts
@@ -1,3 +1,4 @@
+import prisma from "@floe/db";
 import { Probot } from "probot";
 import { handlePushEvents } from "./push-handler";
 
@@ -7,7 +8,14 @@ const appHandler = (app: Probot) => {
    */
   // @ts-ignore
   app.on("push", async (context) => {
-    handlePushEvents(context);
+    // handlePushEvents(context);
+    const project = await prisma.project.findFirst({
+      where: {
+        slug: "floe",
+      },
+    });
+
+    console.log(111111, project);
   });
 };
 
