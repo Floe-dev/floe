d377649a7517328824450eab62caae1aec4613a0
Nicholas Haley <hello@nichaley.com>
2023-09-30 11:59:38 -0400

    Fix path issue

--- apps/web/src/utils/generateURL.ts
+++ apps/web/src/utils/generateURL.ts
@@ -1,12 +1,14 @@
 export function generateURL(
   subdomain: string,
   datasource: string,
-  tab: string,
-  path: string
+  tab?: string,
+  path?: string
 ) {
+  const dynamicPath = [datasource, tab, path].filter(Boolean).join("/");
+
   if (process.env.NODE_ENV === "production") {
-    return `https://${subdomain}.floe.dev/${datasource}/${tab}/${path}`;
+    return `https://${subdomain}.floe.dev/${dynamicPath}`;
   }
 
-  return `http://localhost:3000/${subdomain}/${datasource}/${tab}/${path}`;
+  return `http://localhost:3000/${subdomain}/${dynamicPath}`;
 }
