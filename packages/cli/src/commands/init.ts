090df3519d31ab600c2adb56dc734f48753c1fed
Nicholas Haley <hello@nichaley.com>
2023-10-17 14:58:43 -0400

    Fix bug with building nested and existing pages

--- packages/cli/src/commands/init.ts
+++ packages/cli/src/commands/init.ts
@@ -135,37 +135,49 @@ export function init(program: Command) {
         const sections = allFiles.reduce((acc, file) => {
           const parts = file.split("/");
           // @ts-ignore
-          const createPages = (pages: any[], parts: string[]) => {
+          const createPages = (pages: any[], parts: string[], depth = 0) => {
             const [first, ...rest] = parts;
             const title = capitalize(first.replace(".md", ""));
 
-            if (rest.length === 0) {
-              return {
-                title,
-                pageView: {
-                  path: file.replace(".md", ""),
-                },
-              };
-            }
-
-            const existingSection = pages.find(
+            /**
+             * If page already exists, add to it
+             */
+            const existingPage = pages.find(
               (page) => page.title === title && page.pages
             );
 
-            if (existingSection) {
-              existingSection.pages.push(
-                createPages(existingSection.pages, rest)
+            if (existingPage) {
+              existingPage.pages.push(
+                createPages(existingPage.pages, rest, depth + 1)
               );
               return pages;
             }
 
-            return [
-              ...pages,
-              {
-                title,
-                pages: [createPages([], rest)],
-              },
-            ];
+            // @ts-ignore
+            const page =
+              /**
+               * If page is a leaf node, return a pageView. If not, return a page with pages
+               */
+              rest.length === 0
+                ? {
+                    title,
+                    pageView: {
+                      path: file.replace(".md", ""),
+                    },
+                  }
+                : {
+                    title,
+                    pages: [createPages([], rest, depth + 1)],
+                  };
+
+            /**
+             * If we are at the root, return the pages array
+             */
+            if (depth === 0) {
+              return [...pages, page];
+            }
+
+            return page;
           };
 
           return createPages(acc, parts);
