31aa7490dab22c8c5eb2de41142ced2354ae22a6
Nicholas Haley <hello@nichaley.com>
2023-10-21 21:25:48 -0400

    wip

--- packages/cli/src/commands/add.ts
+++ packages/cli/src/commands/add.ts
@@ -14,26 +14,36 @@ export function add(program: Command) {
     .command("add")
     .description("Add")
     .action(async () => {
-      console.log("Add");
-      // const token = await getAccessToken();
-
-      // const api = createTRPCProxyClient<AppRouter>({
-      //   links: [
-      //     httpBatchLink({
-      //       url: `${getBaseUrl()}/api/trpc`,
-      //       headers: {
-      //         Authorization: token.access_token,
-      //       },
-      //     }),
-      //   ],
-      // });
-
-      // const res = await api.content.generate.query();
-      // console.log(2222, res);
-
       const git = simpleGit();
       const diff = await git.diff();
 
       console.log(11111, diff);
+      const token = await getAccessToken();
+
+      const api = createTRPCProxyClient<AppRouter>({
+        links: [
+          httpBatchLink({
+            url: `${getBaseUrl()}/api/trpc`,
+            headers: {
+              Authorization: token.access_token,
+            },
+          }),
+        ],
+      });
+
+      const template = `
+        ---
+        title: "My first changelog"
+        date: 2023-08-31
+        image: /image1.jpg
+        ---
+        We are excited to announce the release of our new product. It's been a long time coming, but we're finally ready to share it with you!
+      `;
+
+      const res = await api.content.generate.query({
+        diff,
+        template,
+      });
+      console.log(2222, res);
     });
 }
--- packages/trpc/server/router/content.ts
+++ packages/trpc/server/router/content.ts
@@ -1,34 +1,42 @@
-import { protectedTokenProcedure, router } from "../trpc";
+import { z } from "zod";
 import OpenAI from "openai";
+import { protectedTokenProcedure, router } from "../trpc";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 export const contentRouter = router({
-  generate: protectedTokenProcedure.query(async ({}) => {
-    console.log(111111);
+  generate: protectedTokenProcedure
+    .input(
+      z.object({
+        diff: z.string(),
+        template: z.string(),
+      })
+    )
+    .query(async ({ input }) => {
+      console.log(111111);
 
-    try {
-      const response = await openai.chat.completions.create({
-        model: "gpt-3.5-turbo",
-        messages: [
-          { role: "system", content: "You are a helpful assistant." },
-          { role: "user", content: "Who won the world series in 2020?" },
-          {
-            role: "assistant",
-            content: "The Los Angeles Dodgers won the World Series in 2020.",
-          },
-          { role: "user", content: "Where was it played?" },
-        ],
-      });
+      try {
+        const response = await openai.chat.completions.create({
+          model: "gpt-3.5-turbo",
+          messages: [
+            {
+              role: "system",
+              content:
+                "You generate markdown content based on a git diff and an example. Mimic the example as closely as possible.",
+            },
+            { role: "user", content: `diff: ${input.diff}` },
+            { role: "user", content: `example: ${input.template}` },
+          ],
+        });
 
-      console.log(333333, response.choices[0]);
+        console.log(333333, response.choices[0]);
 
-      return response;
-    } catch (error) {
-      console.log(444444, error);
-      return "yodo :(";
-    }
-  }),
+        return response;
+      } catch (error) {
+        console.log(444444, error);
+        return "yodo :(";
+      }
+    }),
 });
