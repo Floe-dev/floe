45803a498afc09ecfe333e8499a3371405b333e0
Nicholas Haley <hello@nichaley.com>
2023-12-04 20:10:38 -0500

    wip - generate embeddings

--- packages/embeddings-action/src/index.ts
+++ packages/embeddings-action/src/index.ts
@@ -1,20 +1,46 @@
 import { getInput, setOutput, setFailed } from "@actions/core";
-import { context } from "@actions/github";
+import { OpenAI } from "openai";
 import { walk } from "./utils/walk";
 
-async function generateEmbeddings({ docsRootPath }: { docsRootPath: string }) {
+async function generateEmbeddings({
+  docsRootPath,
+  openAiKey,
+}: {
+  docsRootPath: string;
+  openAiKey: string;
+}) {
   const embeddingSources = (await walk(docsRootPath)).filter(({ path }) =>
     /\.(md|mdx|mdoc)$/i.test(path)
   );
 
   console.log(333333, embeddingSources);
+  const input = embeddingSources.map(({ path }) => path).join("\n");
+
+  const openai = new OpenAI({
+    apiKey: openAiKey,
+  });
+
+  /**
+   * Step 1: Generate the embeddings
+   */
+  const embeddingResponse = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input,
+  });
+
+  /**
+   * Step 2 (Optional): Store in VectorDB
+   * TODO: Add this later.
+   */
 }
 
 async function run(): Promise<void> {
+  const openAiKey: string = getInput("open-ai-key");
   const docsRootPath: string = getInput("docs-root-path");
 
   try {
     await generateEmbeddings({
+      openAiKey,
       docsRootPath,
     });
   } catch (error) {
