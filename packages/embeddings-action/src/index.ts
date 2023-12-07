be96ebce217f39b2a176d53a7e9b1f5bd538ed30
Nicholas Haley <hello@nichaley.com>
2023-12-07 09:12:50 -0500

    Create first embedding and index

--- packages/embeddings-action/src/index.ts
+++ packages/embeddings-action/src/index.ts
@@ -1,5 +1,9 @@
+import fs from "node:fs";
 import { getInput, setOutput, setFailed } from "@actions/core";
 import { OpenAI } from "openai";
+import { glob } from "glob";
+import { v4 as uuidv4 } from "uuid";
+import { Pinecone } from "@pinecone-database/pinecone";
 import { walk } from "./utils/walk";
 import { getApi } from "./utils/api";
 
@@ -8,38 +12,80 @@ async function generateEmbeddings({
   openAiKey,
   floeApiSecret,
   floeApiWorkspace,
+  pineconeApiKey,
 }: {
   docsRootPath: string;
   openAiKey: string;
   floeApiSecret: string;
   floeApiWorkspace: string;
+  pineconeApiKey: string;
 }) {
-  const api = getApi(floeApiSecret, floeApiWorkspace);
-  const embeddingSources = (await walk(docsRootPath)).filter(({ path }) =>
-    /\.(md|mdx|mdoc)$/i.test(path)
-  );
+  // const api = getApi(floeApiSecret, floeApiWorkspace);
+  // const embeddingSources = (await walk(docsRootPath)).filter(({ path }) =>
+  //   /\.(md|mdx|mdoc)$/i.test(path)
+  // );
+  const files = await glob(`${docsRootPath}/**/*.{md,mdx, mdoc}`, {
+    ignore: "node_modules/**",
+  });
+  const embeddingSources = files.map((path) => {
+    const article = fs.readFileSync(path, { encoding: "utf8" });
 
-  console.log(333333, embeddingSources);
-  const input = embeddingSources.map(({ path }) => path).join("\n");
+    return {
+      path,
+      article,
+    };
+  });
 
   const openai = new OpenAI({
     apiKey: openAiKey,
   });
 
-  /**
-   * Step 1: Generate the embeddings
-   */
-  const embeddingResponse = await openai.embeddings.create({
-    model: "text-embedding-ada-002",
-    input,
+  const pinecone = new Pinecone({
+    environment: "northamerica-northeast1-gcp",
+    apiKey: pineconeApiKey,
   });
 
-  /**
-   * Step 2: Store embeddings
-   */
-  await api.post("/store-embeddings", {
-    embeddings: embeddingResponse.data,
+  const index = pinecone.Index("floe");
+
+  const ns = index.namespace("my-first-namespace");
+
+  console.log(1111111, embeddingSources);
+
+  embeddingSources.forEach(async ({ path, article }, i: number) => {
+    const id = uuidv4();
+
+    /**
+     * Step 1: Generate the embeddings
+     */
+    const embeddingResponse = await openai.embeddings.create({
+      model: "text-embedding-ada-002",
+      input: article,
+    });
+
+    await ns.upsert([
+      {
+        id,
+        values: embeddingResponse.data[0].embedding,
+        metadata: {
+          content: article,
+          contentTokens: embeddingResponse.usage.total_tokens,
+        },
+      },
+    ]);
+
+    /**
+     * Step 2: Store embeddings
+     */
+    // await api.post("/store-embeddings", {
+    //   embeddings: embeddingResponse.data,
+    // });
   });
+
+  // const input = embeddingSources.map(({ path }) => path).join("\n");
+
+  return;
+
+  // console.log(1111111, embeddingResponse);
 }
 
 async function run(): Promise<void> {
@@ -47,6 +93,7 @@ async function run(): Promise<void> {
   const docsRootPath: string = getInput("docs-root-path");
   const floeApiSecret: string = getInput("floe-api-secret");
   const floeApiWorkspace: string = getInput("floe-api-workspace");
+  const pineconeApiKey: string = getInput("pinecone-api-key");
 
   try {
     await generateEmbeddings({
@@ -54,6 +101,7 @@ async function run(): Promise<void> {
       docsRootPath,
       floeApiSecret,
       floeApiWorkspace,
+      pineconeApiKey,
     });
   } catch (error) {
     if (error instanceof Error) setFailed(error.message);
