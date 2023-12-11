465b84d2e7d5a306ff371ad864fb78bb83c869bf
Nicholas Haley <hello@nichaley.com>
2023-12-11 10:29:47 -0500

    Fixes for next config

--- apps/app/next.config.mjs
+++ apps/app/next.config.mjs
@@ -1,15 +1,15 @@
+import { PrismaPlugin } from "@prisma/nextjs-monorepo-workaround-plugin";
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
 await import("./src/env.mjs");
-const { PrismaPlugin } = require("@prisma/nextjs-monorepo-workaround-plugin");
 
 /** @type {import("next").NextConfig} */
 const config = {
-  webpack: (config, { isServer }) => {
+  webpack: (c, { isServer }) => {
     if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()];
+      c.plugins = [...c.plugins, new PrismaPlugin()];
     }
 
     return config;
