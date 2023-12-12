a07cd4fff93470d7906ce945d49726985a515196
Nicholas Haley <hello@nichaley.com>
2023-12-12 11:26:40 -0500

    Add signout button

--- apps/app/src/app/(authenticated)/[workspace]/nav.tsx
+++ apps/app/src/app/(authenticated)/[workspace]/nav.tsx
@@ -1,7 +1,7 @@
 "use client";
 import logo from "public/logo.png";
 import type { Prisma } from "@floe/db";
-import { Dialog, Transition } from "@headlessui/react";
+import { Menu, Dialog, Transition } from "@headlessui/react";
 import { classNames } from "@floe/lib/class-names";
 import Image from "next/image";
 import { Bars3Icon, HomeIcon, XMarkIcon } from "@heroicons/react/24/outline";
@@ -49,7 +49,7 @@ export function Nav({
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <div className="fixed inset-0 bg-gray-900/80" />
+            <div className="fixed inset-0 bg-zinc-900/80" />
           </Transition.Child>
 
           <div className="fixed inset-0 flex">
@@ -107,8 +107,8 @@ export function Nav({
                               <a
                                 className={classNames(
                                   item.current
-                                    ? "bg-gray-50 text-amber-600"
-                                    : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
+                                    ? "bg-zinc-50 text-amber-600"
+                                    : "text-zinc-700 hover:text-amber-600 hover:bg-zinc-50",
                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 )}
                                 href={item.href}
@@ -118,7 +118,7 @@ export function Nav({
                                   className={classNames(
                                     item.current
                                       ? "text-amber-600"
-                                      : "text-gray-400 group-hover:text-amber-600",
+                                      : "text-zinc-400 group-hover:text-amber-600",
                                     "h-6 w-6 shrink-0"
                                   )}
                                 />
@@ -129,7 +129,7 @@ export function Nav({
                         </ul>
                       </li>
                       <li>
-                        <div className="text-xs font-semibold leading-6 text-gray-400">
+                        <div className="text-xs font-semibold leading-6 text-zinc-400">
                           Your workspaces
                         </div>
                         <ul className="mt-2 -mx-2 space-y-1">
@@ -138,8 +138,8 @@ export function Nav({
                               <a
                                 className={classNames(
                                   ws.slug === workspace
-                                    ? "bg-gray-50 text-amber-600"
-                                    : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
+                                    ? "bg-zinc-50 text-amber-600"
+                                    : "text-zinc-700 hover:text-amber-600 hover:bg-zinc-50",
                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 )}
                                 href={ws.slug}
@@ -148,7 +148,7 @@ export function Nav({
                                   className={classNames(
                                     ws.slug === workspace
                                       ? "text-amber-600 border-amber-600"
-                                      : "text-gray-400 border-gray-200 group-hover:border-amber-600 group-hover:text-amber-600",
+                                      : "text-zinc-400 border-zinc-200 group-hover:border-amber-600 group-hover:text-amber-600",
                                     "capitalize flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
                                   )}
                                 >
@@ -172,7 +172,7 @@ export function Nav({
       {/* Static sidebar for desktop */}
       <div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col">
         {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className="flex flex-col px-6 overflow-y-auto bg-white border-r border-gray-200 grow gap-y-5">
+        <div className="flex flex-col px-6 overflow-y-auto bg-white border-r border-zinc-200 grow gap-y-5">
           <div className="flex items-center h-16 shrink-0">
             <Image
               alt="Floe logo"
@@ -190,8 +190,8 @@ export function Nav({
                       <a
                         className={classNames(
                           item.current
-                            ? "bg-gray-50 text-amber-600"
-                            : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
+                            ? "bg-zinc-50 text-amber-600"
+                            : "text-zinc-700 hover:text-amber-600 hover:bg-zinc-50",
                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                         )}
                         href={item.href}
@@ -201,7 +201,7 @@ export function Nav({
                           className={classNames(
                             item.current
                               ? "text-amber-600"
-                              : "text-gray-400 group-hover:text-amber-600",
+                              : "text-zinc-400 group-hover:text-amber-600",
                             "h-6 w-6 shrink-0"
                           )}
                         />
@@ -212,7 +212,7 @@ export function Nav({
                 </ul>
               </li>
               <li>
-                <div className="text-xs font-semibold leading-6 text-gray-400">
+                <div className="text-xs font-semibold leading-6 text-zinc-400">
                   Your workspaces
                 </div>
                 <ul className="mt-2 -mx-2 space-y-1">
@@ -221,8 +221,8 @@ export function Nav({
                       <a
                         className={classNames(
                           ws.slug === workspace
-                            ? "bg-gray-50 text-amber-600"
-                            : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
+                            ? "bg-zinc-50 text-amber-600"
+                            : "text-zinc-700 hover:text-amber-600 hover:bg-zinc-50",
                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                         )}
                         href={ws.slug}
@@ -231,7 +231,7 @@ export function Nav({
                           className={classNames(
                             ws.slug === workspace
                               ? "text-amber-600 border-amber-600"
-                              : "text-gray-400 border-gray-200 group-hover:border-amber-600 group-hover:text-amber-600",
+                              : "text-zinc-400 border-zinc-200 group-hover:border-amber-600 group-hover:text-amber-600",
                             "capitalize flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
                           )}
                         >
@@ -244,18 +244,45 @@ export function Nav({
                 </ul>
               </li>
               <li className="mt-auto -mx-6">
-                <a
-                  className="flex items-center px-6 py-3 text-sm font-semibold leading-6 text-gray-900 gap-x-4 hover:bg-gray-50"
-                  href="#"
-                >
-                  <img
-                    alt=""
-                    className="w-8 h-8 rounded-full bg-gray-50"
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  />
-                  <span className="sr-only">Your profile</span>
-                  <span aria-hidden="true">Tom Cook</span>
-                </a>
+                <Menu as="div" className="relative">
+                  <Menu.Button className="flex items-center w-full px-6 py-3 text-sm font-semibold leading-6 text-zinc-900 gap-x-4 hover:bg-zinc-50">
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      alt=""
+                      className="w-8 h-8 rounded-full bg-zinc-50"
+                      src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                    />
+                    <span className="hidden lg:flex lg:items-center">
+                      <span
+                        aria-hidden="true"
+                        className="ml-4 text-sm font-semibold leading-6 text-zinc-900"
+                      >
+                        Tom Cook
+                      </span>
+                    </span>
+                  </Menu.Button>
+                  <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                  >
+                    <Menu.Items className="absolute bottom-full left-6 z-10 mb-2.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-zinc-900/5 focus:outline-none">
+                      <Menu.Item>
+                        <button
+                          className="block px-3 py-1 text-sm leading-6 text-zinc-900"
+                          onClick={() => signOut()}
+                          type="button"
+                        >
+                          Sign out
+                        </button>
+                      </Menu.Item>
+                    </Menu.Items>
+                  </Transition>
+                </Menu>
               </li>
             </ul>
           </nav>
@@ -264,7 +291,7 @@ export function Nav({
 
       <div className="sticky top-0 z-40 flex items-center px-4 py-4 bg-white shadow-sm gap-x-6 sm:px-6 lg:hidden">
         <button
-          className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
+          className="-m-2.5 p-2.5 text-zinc-700 lg:hidden"
           onClick={() => {
             setSidebarOpen(true);
           }}
@@ -273,14 +300,14 @@ export function Nav({
           <span className="sr-only">Open sidebar</span>
           <Bars3Icon aria-hidden="true" className="w-6 h-6" />
         </button>
-        <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
+        <div className="flex-1 text-sm font-semibold leading-6 text-zinc-900">
           Dashboard
         </div>
         <a href="#">
           <span className="sr-only">Your profile</span>
           <img
             alt=""
-            className="w-8 h-8 rounded-full bg-gray-50"
+            className="w-8 h-8 rounded-full bg-zinc-50"
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
           />
         </a>
