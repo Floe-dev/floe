fff5f507d532b6f9366f2da1f404e1e72b51b3ee
Nicholas Haley <hello@nichaley.com>
2023-12-12 09:54:14 -0500

    wip - cleaning up nav

--- apps/app/src/app/(authenticated)/nav.tsx
+++ apps/app/src/app/(authenticated)/nav.tsx
@@ -1,37 +1,23 @@
 "use client";
-import { Dialog, Menu, Transition } from "@headlessui/react";
+import logo from "public/logo.png";
+import { Dialog, Transition } from "@headlessui/react";
+import { classNames } from "@floe/lib/class-names";
+import Image from "next/image";
 import {
   Bars3Icon,
-  BellIcon,
   CalendarIcon,
   ChartPieIcon,
-  Cog6ToothIcon,
   DocumentDuplicateIcon,
   FolderIcon,
   HomeIcon,
   UsersIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline";
-import {
-  ChevronDownIcon,
-  MagnifyingGlassIcon,
-} from "@heroicons/react/20/solid";
-import { Fragment } from "react";
-
+import { Fragment, useState } from "react";
 import { signOut } from "next-auth/react";
-import { useState } from "react";
-
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
 
 const navigation = [
   { name: "Dashboard", href: "#", icon: HomeIcon, current: true },
-  { name: "Team", href: "#", icon: UsersIcon, current: false },
-  { name: "Projects", href: "#", icon: FolderIcon, current: false },
-  { name: "Calendar", href: "#", icon: CalendarIcon, current: false },
-  { name: "Documents", href: "#", icon: DocumentDuplicateIcon, current: false },
-  { name: "Reports", href: "#", icon: ChartPieIcon, current: false },
 ];
 const teams = [
   { id: 1, name: "Heroicons", href: "#", initial: "H", current: false },
@@ -48,7 +34,7 @@ export function Nav() {
 
   return (
     <div>
-      <Transition.Root show={sidebarOpen} as={Fragment}>
+      <Transition.Root as={Fragment} show={sidebarOpen}>
         <Dialog
           as="div"
           className="relative z-50 lg:hidden"
@@ -88,14 +74,16 @@ export function Nav() {
                 >
                   <div className="absolute top-0 flex justify-center w-16 pt-5 left-full">
                     <button
-                      type="button"
                       className="-m-2.5 p-2.5"
-                      onClick={() => setSidebarOpen(false)}
+                      onClick={() => {
+                        setSidebarOpen(false);
+                      }}
+                      type="button"
                     >
                       <span className="sr-only">Close sidebar</span>
                       <XMarkIcon
-                        className="w-6 h-6 text-white"
                         aria-hidden="true"
+                        className="w-6 h-6 text-white"
                       />
                     </button>
                   </div>
@@ -103,35 +91,36 @@ export function Nav() {
                 {/* Sidebar component, swap this element with another sidebar if you like */}
                 <div className="flex flex-col px-6 pb-2 overflow-y-auto bg-white grow gap-y-5">
                   <div className="flex items-center h-16 shrink-0">
-                    <img
-                      className="w-auto h-8"
-                      src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                      alt="Your Company"
+                    <Image
+                      alt="Floe logo"
+                      className="w-auto h-6"
+                      placeholder="blur"
+                      src={logo}
                     />
                   </div>
                   <nav className="flex flex-col flex-1">
-                    <ul role="list" className="flex flex-col flex-1 gap-y-7">
+                    <ul className="flex flex-col flex-1 gap-y-7">
                       <li>
-                        <ul role="list" className="-mx-2 space-y-1">
+                        <ul className="-mx-2 space-y-1">
                           {navigation.map((item) => (
                             <li key={item.name}>
                               <a
-                                href={item.href}
                                 className={classNames(
                                   item.current
-                                    ? "bg-gray-50 text-indigo-600"
-                                    : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                    ? "bg-gray-50 text-amber-600"
+                                    : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 )}
+                                href={item.href}
                               >
                                 <item.icon
+                                  aria-hidden="true"
                                   className={classNames(
                                     item.current
-                                      ? "text-indigo-600"
-                                      : "text-gray-400 group-hover:text-indigo-600",
+                                      ? "text-amber-600"
+                                      : "text-gray-400 group-hover:text-amber-600",
                                     "h-6 w-6 shrink-0"
                                   )}
-                                  aria-hidden="true"
                                 />
                                 {item.name}
                               </a>
@@ -143,23 +132,23 @@ export function Nav() {
                         <div className="text-xs font-semibold leading-6 text-gray-400">
                           Your teams
                         </div>
-                        <ul role="list" className="mt-2 -mx-2 space-y-1">
+                        <ul className="mt-2 -mx-2 space-y-1">
                           {teams.map((team) => (
                             <li key={team.name}>
                               <a
-                                href={team.href}
                                 className={classNames(
                                   team.current
-                                    ? "bg-gray-50 text-indigo-600"
-                                    : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                                    ? "bg-gray-50 text-amber-600"
+                                    : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
                                   "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                                 )}
+                                href={team.href}
                               >
                                 <span
                                   className={classNames(
                                     team.current
-                                      ? "text-indigo-600 border-indigo-600"
-                                      : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                                      ? "text-amber-600 border-amber-600"
+                                      : "text-gray-400 border-gray-200 group-hover:border-amber-600 group-hover:text-amber-600",
                                     "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
                                   )}
                                 >
@@ -185,35 +174,36 @@ export function Nav() {
         {/* Sidebar component, swap this element with another sidebar if you like */}
         <div className="flex flex-col px-6 overflow-y-auto bg-white border-r border-gray-200 grow gap-y-5">
           <div className="flex items-center h-16 shrink-0">
-            <img
-              className="w-auto h-8"
-              src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-              alt="Your Company"
+            <Image
+              alt="Floe logo"
+              className="w-auto h-6"
+              placeholder="blur"
+              src={logo}
             />
           </div>
           <nav className="flex flex-col flex-1">
-            <ul role="list" className="flex flex-col flex-1 gap-y-7">
+            <ul className="flex flex-col flex-1 gap-y-7">
               <li>
-                <ul role="list" className="-mx-2 space-y-1">
+                <ul className="-mx-2 space-y-1">
                   {navigation.map((item) => (
                     <li key={item.name}>
                       <a
-                        href={item.href}
                         className={classNames(
                           item.current
-                            ? "bg-gray-50 text-indigo-600"
-                            : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                            ? "bg-gray-50 text-amber-600"
+                            : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                         )}
+                        href={item.href}
                       >
                         <item.icon
+                          aria-hidden="true"
                           className={classNames(
                             item.current
-                              ? "text-indigo-600"
-                              : "text-gray-400 group-hover:text-indigo-600",
+                              ? "text-amber-600"
+                              : "text-gray-400 group-hover:text-amber-600",
                             "h-6 w-6 shrink-0"
                           )}
-                          aria-hidden="true"
                         />
                         {item.name}
                       </a>
@@ -225,23 +215,23 @@ export function Nav() {
                 <div className="text-xs font-semibold leading-6 text-gray-400">
                   Your teams
                 </div>
-                <ul role="list" className="mt-2 -mx-2 space-y-1">
+                <ul className="mt-2 -mx-2 space-y-1">
                   {teams.map((team) => (
                     <li key={team.name}>
                       <a
-                        href={team.href}
                         className={classNames(
                           team.current
-                            ? "bg-gray-50 text-indigo-600"
-                            : "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
+                            ? "bg-gray-50 text-amber-600"
+                            : "text-gray-700 hover:text-amber-600 hover:bg-gray-50",
                           "group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
                         )}
+                        href={team.href}
                       >
                         <span
                           className={classNames(
                             team.current
-                              ? "text-indigo-600 border-indigo-600"
-                              : "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
+                              ? "text-amber-600 border-amber-600"
+                              : "text-gray-400 border-gray-200 group-hover:border-amber-600 group-hover:text-amber-600",
                             "flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
                           )}
                         >
@@ -255,13 +245,13 @@ export function Nav() {
               </li>
               <li className="mt-auto -mx-6">
                 <a
-                  href="#"
                   className="flex items-center px-6 py-3 text-sm font-semibold leading-6 text-gray-900 gap-x-4 hover:bg-gray-50"
+                  href="#"
                 >
                   <img
+                    alt=""
                     className="w-8 h-8 rounded-full bg-gray-50"
                     src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    alt=""
                   />
                   <span className="sr-only">Your profile</span>
                   <span aria-hidden="true">Tom Cook</span>
@@ -274,12 +264,14 @@ export function Nav() {
 
       <div className="sticky top-0 z-40 flex items-center px-4 py-4 bg-white shadow-sm gap-x-6 sm:px-6 lg:hidden">
         <button
-          type="button"
           className="-m-2.5 p-2.5 text-gray-700 lg:hidden"
-          onClick={() => setSidebarOpen(true)}
+          onClick={() => {
+            setSidebarOpen(true);
+          }}
+          type="button"
         >
           <span className="sr-only">Open sidebar</span>
-          <Bars3Icon className="w-6 h-6" aria-hidden="true" />
+          <Bars3Icon aria-hidden="true" className="w-6 h-6" />
         </button>
         <div className="flex-1 text-sm font-semibold leading-6 text-gray-900">
           Dashboard
@@ -287,9 +279,9 @@ export function Nav() {
         <a href="#">
           <span className="sr-only">Your profile</span>
           <img
+            alt=""
             className="w-8 h-8 rounded-full bg-gray-50"
             src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
           />
         </a>
       </div>
