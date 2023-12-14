8dc0637d40031493356ef8482d602ff5c8399963
Nicholas Haley <hello@nichaley.com>
2023-12-14 13:45:16 -0500

    Add new favicon, some cleanup

--- apps/app/src/app/(authenticated)/new/nav.tsx
+++ apps/app/src/app/(authenticated)/new/nav.tsx
@@ -7,12 +7,7 @@ import { signOut } from "next-auth/react";
 export function Nav() {
   return (
     <nav className="flex justify-between p-6">
-      <Image
-        alt="Floe logo"
-        className="w-auto h-6"
-        placeholder="blur"
-        src={logo}
-      />
+      <Image alt="Floe logo" className="w-auto h-6" src={logo} />
       <button
         className="self-start text-sm text-zinc-500"
         onClick={() => signOut()}
