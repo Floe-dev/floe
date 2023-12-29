674a52b931b352351194b961ba36b1e1f2db381f
Nicholas Haley <hello@nichaley.com>
2023-12-29 16:24:43 -0500

    Add fields to Workspace for Stripe

--- apps/app/src/app/(authenticated)/[workspace]/settings/page.tsx
+++ apps/app/src/app/(authenticated)/[workspace]/settings/page.tsx
@@ -1,7 +1,15 @@
 export default function Settings() {
   return (
     <div>
-      <h1>Settings</h1>
+      <div className="prose prose-zinc">
+        <h2 className="mb-2">Settings</h2>
+        <p>Manage your billing.</p>
+        <h3>Billing</h3>
+        <form action="/create-checkout-session" method="POST">
+          <input name="priceId" type="hidden" value="price_G0FvDp6vZvdwRZ" />
+          <button type="submit">Checkout</button>
+        </form>
+      </div>
     </div>
   );
 }
