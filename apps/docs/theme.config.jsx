18cb94f6056f025481019207225b68ba3aa46933
Nicholas Haley <hello@nichaley.com>
2023-12-31 17:47:24 -0500

    Add favicon to Floe docs

--- apps/docs/theme.config.jsx
+++ apps/docs/theme.config.jsx
@@ -1,7 +1,26 @@
+import { useConfig } from "nextra-theme-docs";
+
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>Floe Documentation</span>,
   project: {
-    link: "https://github.com/shuding/nextra",
+    link: "https://github.com/Floe-dev/floe",
   },
-  // ... other theme options
+  head: function useHead() {
+    const { title } = useConfig();
+
+    return (
+      <>
+        <meta name="msapplication-TileColor" content="#fff" />
+        <meta name="theme-color" content="#fff" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="description" content="Floe documentation." />
+        <meta name="og:description" content="Floe documentation." />
+
+        <meta name="og:title" content={title ? title + " – Floe" : "Floe"} />
+        <meta name="apple-mobile-web-app-title" content="Floe" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </>
+    );
+  },
 };
