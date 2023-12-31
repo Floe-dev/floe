79baaff7bef7f01ab11cc45c28c86d5c7bf28514
Nicholas Haley <hello@nichaley.com>
2023-12-31 15:12:15 -0500

    Creating next steps list

--- apps/app/src/app/(authenticated)/[workspace]/page.tsx
+++ apps/app/src/app/(authenticated)/[workspace]/page.tsx
@@ -48,9 +48,39 @@ export default async function Workspace({
   return (
     <div>
       <div className="mb-10 prose prose-zinc">
-        <h2 className="mb-2">Welcome back</h2>
+        <h2 className="mb-2">Dashboard</h2>
         <p>Manage your keys and integrations.</p>
       </div>
+      {/* Next steps */}
+      <div className="px-6 py-5 mb-4 prose bg-white rounded-lg shadow prose-zinc">
+        <h3 className="text-base font-semibold leading-6 text-gray-900">
+          Next steps
+        </h3>
+        <p className="mt-2 text-sm text-gray-500">Complete your Floe setup.</p>
+        <ul className="p-0 list-none">
+          <li className="flex items-center gap-x-4">
+            <div className="flex items-center justify-center w-6 h-6 m-0 text-sm rounded-full bg-amber-100 text-amber-800">
+              1
+            </div>
+            <p className="m-0">
+              Generate an API key and store it somewhere safe.
+            </p>
+          </li>
+          <li className="flex items-center gap-x-4">
+            <div className="flex items-center justify-center w-6 h-6 m-0 text-sm rounded-full bg-amber-100 text-amber-800">
+              2
+            </div>
+            <p className="m-0">Connect to GitHub. (Optional)</p>
+          </li>
+          <li className="flex items-center gap-x-4">
+            <div className="flex items-center justify-center w-6 h-6 m-0 text-sm rounded-full bg-amber-100 text-amber-800">
+              3
+            </div>
+            <p className="m-0">Install the CLI and initialize Floe.</p>
+          </li>
+        </ul>
+      </div>
+
       <AuthConnect workspace={workspace} />
       <Keys workspace={workspace} />
     </div>
