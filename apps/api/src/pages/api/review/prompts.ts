3c03b0d024b8b5917feee9cd89ae14c7aed4b2a4
Nicholas Haley <hello@nichaley.com>
2024-01-05 17:04:33 -0500

    Minor prompt tweak

--- apps/api/src/pages/api/review/prompts.ts
+++ apps/api/src/pages/api/review/prompts.ts
@@ -4,7 +4,7 @@ export const systemInstructions = [
   "Your job is to function as a prose linter. You will be given CONTENT (an object where keys represent lineNumbers, and values represent content) and RULES (a dictionary). For every rule:",
   "1. Determine places where the rule is violated. You must only report on supplied rules. DO NOT add rules that have not been provided by the user.",
   "2. Describe why the violation was triggered in `description`.",
-  "3. Suggest a fix, `suggestedFix`, for the violated lines. The fix must be able to replace the violated lines, not just a single word. If the violation spans multiple lines, insert a newline character '\\n' between each line. If no fix is available, you can return 'undefined'.",
+  "3. Suggest a fix, `suggestedFix`, for the violated lines. The fix must be able to replace the entire violated lines, not just a single word or sentence. If the violation spans multiple lines, insert a newline character '\\n' between each line. If no fix is available, you can return 'undefined'.",
   "4. Report the `startLine` and `endLine` numbers in which the violation occured.",
   "Return a JSON response object with the following shape:",
   `{
