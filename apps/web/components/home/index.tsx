ed3e3519a6abcc34fdbcb72ef29aef6f2f8805d6
Nicholas Haley <hello@nichaley.com>
2024-01-11 08:54:23 -0500

    Fix title sizing

--- apps/web/components/home/index.tsx
+++ apps/web/components/home/index.tsx
@@ -19,8 +19,10 @@ export default function Home(): JSX.Element {
     <div className="relative overflow-x-hidden bg-white isolate bg-noise after:bg-gradient-to-t after:from-zinc-900 after:opacity-20 after:-z-30 after:absolute after:inset-0">
       <div className="px-6 pt-20 mx-auto max-w-7xl sm:pt-28 lg:px-8 lg:pt-32">
         <section className="lg:flex lg:items-center lg:gap-x-10">
-          <div className="max-w-2xl mx-auto lg:mx-0 lg:flex-auto">
-            <Pill text="Coming soon" />
+          <div className="flex flex-col max-w-2xl mx-auto lg:mx-0 lg:flex-auto">
+            <span className="flex-grow-0">
+              <Pill text="Coming soon" />
+            </span>
             <Title />
             <p className="mt-4 mb-10 text-xl leading-8 sm:text-2xl sm:mt-6 sm:mb-10 text-zinc-600">
               Meet your new AI-powered writing assistant. Floe is the CICD
