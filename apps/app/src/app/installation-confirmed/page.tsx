fecd50cb5638ca30493d52234aa50ff2bc0fbadb
Nicholas Haley <hello@nichaley.com>
2024-01-14 15:55:05 -0500

    Improve styling for installation confirmed page

--- apps/app/src/app/installation-confirmed/page.tsx
+++ apps/app/src/app/installation-confirmed/page.tsx
@@ -1,10 +1,14 @@
+import { CheckCircleIcon } from "@heroicons/react/24/solid";
+
 export default function Page() {
   return (
     <div className="px-4 prose prose-zinc sm:w-full sm:max-w-[360px] mx-auto pt-60 flex flex-col items-center">
+      <CheckCircleIcon className="h-8" />
+      <h3>Your GitHub App has been installed!</h3>
       <p>
-        Your GitHub App has been installed! For security reasons your app will
-        go through an additional approval step before it is activated with Floe.
-        This usually takes less than 24 hours.
+        For security reasons your app will go through an additional approval
+        step before it is activated with Floe. This usually takes less than 24
+        hours.
       </p>
     </div>
   );
