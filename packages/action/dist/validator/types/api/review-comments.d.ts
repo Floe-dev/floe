7b2eb675e65e9490e518a454434844a1bb9243da
Nicholas Haley <hello@nichaley.com>
2024-01-15 14:09:07 -0500

    Remove outdated action

--- packages/action/dist/validator/types/api/review-comments.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Endpoints } from "@octokit/types";
-export type GetGitReviewCommentsResponse =
-  Endpoints["GET /repos/{owner}/{repo}/pulls/{pull_number}/comments"]["response"]["data"];
-export type PostGitReviewCommentsResponse =
-  Endpoints["POST /repos/{owner}/{repo}/pulls/{pull_number}/comments"]["response"]["data"];
-export type PatchGitReviewCommentsResponse =
-  Endpoints["PATCH /repos/{owner}/{repo}/pulls/comments/{comment_id}"]["response"]["data"];
-//# sourceMappingURL=review-comments.d.ts.map
--- packages/action/src/validator/types.ts
+++
@@ -1,3 +0,0 @@
-export interface Inputs {
-  token: string;
-}
