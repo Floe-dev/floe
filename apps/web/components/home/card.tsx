9783747d76d527b78de597ae8058debb60a1498c
Nicholas Haley <hello@nichaley.com>
2024-01-22 20:35:36 -0500

    Make mobile icons larger

--- apps/web/components/home/card.tsx
+++ apps/web/components/home/card.tsx
@@ -16,7 +16,7 @@ export function Card({
 
   return (
     <div className="relative z-10 px-10 py-8 bg-white rounded-lg shadow-lg bg-opacity-70">
-      <Icon className="h-6 mb-3 md:h-10 text-zinc-800" />
+      <Icon className="h-8 mb-3 md:h-10 text-zinc-800" />
       <h3 className="mb-3 text-2xl sm:text-3xl font-garamond">{title}</h3>
       <p className="text-md sm:text-lg text-zinc-600">{description}</p>
     </div>
